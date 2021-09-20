15d195520972c923202a269d8376bc4f02864250
KonstantinKirillOff <53314883+KonstantinKirillOff@users.noreply.github.com>
2021-09-20 16:04:10 +0300

    add logic for work with virtual keyboard

--- keyboardTrening/ViewController.swift
+++ keyboardTrening/ViewController.swift
@@ -9,11 +9,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet weak var loginTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
     }
-
-
 }
+
+extension ViewController {
+    func showAlert(_ mess: String) {
+        let alert = UIAlertController(title: "Хотим открыть окно", message: mess, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .cancel)
+        
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+    
+    @objc private func didTapDone() {
+        view.endEditing(true)
+    }
+}
+
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == loginTF {
+            passwordTF.becomeFirstResponder()
+        } else {
+            showAlert("Нажали Return")
+        }
+        return true
+    }
+    
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        let keyboardToolbar = UIToolbar()
+        keyboardToolbar.sizeToFit()
+        textField.inputAccessoryView = keyboardToolbar
+        
+        let doneButton = UIBarButtonItem(
+            barButtonSystemItem: .done,
+            target: self,
+            action: #selector(didTapDone)
+        )
+        
+        let flexBarButton = UIBarButtonItem(
+            barButtonSystemItem: .flexibleSpace,
+            target: nil,
+            action: nil
+        )
+        
+        keyboardToolbar.items = [flexBarButton, doneButton]
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        showAlert("Нажали Done на таб баре")
+    }
+}
+
 
